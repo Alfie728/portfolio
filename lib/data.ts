98baacb2c1fc10f8abde33f85ec986f93596134d
Alfie728 <zegro728@gmail.com>
2024-09-08 18:56:42 +1000

    fixed section title

--- lib/data.ts
+++ lib/data.ts
@@ -24,8 +24,8 @@ export const links = [
     hash: "#skills",
   },
   {
-    name: "Experience",
-    hash: "#experience",
+    name: "Education",
+    hash: "#Education",
   },
   {
     name: "Contact",
